72f6bc025eeaf1b58f8bf665d28d2a4d6523d3ac
winstxnhdw <winstxnhdw@gmail.com>
2023-03-26 03:07:36 +0800

    feat: use config wrapper to access `User` section

--- Scripts/Config/Config.cs
+++ Scripts/Config/Config.cs
@@ -5,8 +5,17 @@ using Microsoft.Extensions.Configuration;
 
 namespace TelMe;
 
-public class ConfigLoader {
-    public static IConfigurationRoot Load() =>
+public class Config {
+    IConfigurationRoot ConfigRoot { get; }
+
+    public Config() {
+        this.ConfigRoot = Config.Load();
+    }
+
+    public T GetValue<T>(string key) => this.ConfigRoot.GetValue<T>($"User:{key}");
+
+    static IConfigurationRoot Load() =>
         new ConfigurationBuilder().AddIniFile($"{Path.GetDirectoryName(Environment.ProcessPath)}/settings.ini")
                                   .Build();
+
 }
